de477cc3acdb2cefba4a8fe34803b1ff1d301be5
Batuhan Gundogdu <63497830+batuhan-gundogdu@users.noreply.github.com>
2022-01-16 17:14:21 -0600

    option to merge all noise types

--- step2_mix_and_create_dataset.py
+++ step2_mix_and_create_dataset.py
@@ -6,13 +6,14 @@ import tensorflow as tf
 
 import argparse
 
+noise_type_helper = '0: all, 1: white, 2: siren, 3 : baby'
 parser = argparse.ArgumentParser(description='Speech Enhancement for Hearing Aid Devices')
-parser.add_argument('--noise_type', type=int, default=0, help='0: white noise, 1: siren, 2: baby')
+parser.add_argument('--noise_type', type=int, default=0, help=noise_type_helper)
 parser.add_argument('--clean_file_no', type=int, default=0, help='select file source')
 
 args = parser.parse_args()
 
-noise_type = {0 : 'white', 1 : 'siren', 2 : 'baby'}
+noise_type = {1 : 'white', 2 : 'siren', 3 : 'baby'}
 
 #create paths
 raw_folder_speech = os.path.join(DATADIR, 'raw_speech')
@@ -20,14 +21,22 @@ raw_folder_noise = os.path.join(DATADIR, 'raw_noise')
 processed_folder = os.path.join(DATADIR, 'processed')
 makedir_exist_ok(processed_folder)
 speech_file = os.path.join(raw_folder_speech, "speech_" + str(args.clean_file_no) + ".pt")
-noise_file = os.path.join(raw_folder_noise, noise_type[args.noise_type] + '_noise.pt')
+if not args.noise_type:
+    noise_file = os.path.join(raw_folder_noise, noise_type[args.noise_type] + '_noise.pt')
+    if os.path.exists(noise_file):
+        noise_set = [torch.load(noise_file)]
+else:
+    noise_set = []
+    for f in os.listdir(raw_folder_noise):
+        if f.endswith('.pt'):
+            noise_file = os.path.join(raw_folder_noise, f)
+            noise_set.append(torch.load(noise_file))
+    print(len(noise_set))
+
 test_file = os.path.join(raw_folder_speech, 'test.pt')
 data_file = 'dataset-speech.pt'
 if os.path.exists(speech_file):
 	clean = torch.load(speech_file)
-
-if os.path.exists(noise_file):
-	noise = torch.load(noise_file)
     
 if os.path.exists(test_file):
 	test = torch.load(test_file)
@@ -40,27 +49,29 @@ noise_out = []
 mask_out = []
 data_types = []
 for count, data_frame in tqdm(enumerate(clean)):
-    noise_start = random.randint(0, noise.shape[0]- data_frame.shape[0])
-    noise_snippet = noise[noise_start: noise_start + data_frame.shape[0]]
-    noisy_frame, valid, scaled_noise = snr_mixer(data_frame, noise_snippet, SNR)
-    if valid:
-        noisy_feat = np.abs(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        noisy_angle = np.angle(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        clean_feat = np.abs(librosa.stft(data_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        noise_feat = np.abs(librosa.stft(scaled_noise, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        # normalize to 0-1.0
-        noisy_feat = noisy_feat / np.amax(noisy_feat)
-        clean_feat = clean_feat / np.amax(clean_feat)
-        noise_feat = noise_feat / np.amax(noise_feat)
-        mask =  tf.truediv(clean_feat, noisy_feat)
-        feat_in.append(quantize_spectrum(noisy_feat))
-        feat_out.append(quantize_spectrum(clean_feat))
-        noise_out.append(quantize_spectrum(noise_feat))
-        # is the recoding used in training (0) or test (1)
-        data_type = (np.random.rand(1) + 0.1).astype(int)
-        data_types.append(data_type)
-        angles.append(noisy_angle)
-        mask_out.append(quantize_spectrum(mask))
+    for noise in noise_set:
+        noise_start = random.randint(0, noise.shape[0]- data_frame.shape[0])
+        noise_snippet = noise[noise_start: noise_start + data_frame.shape[0]]
+        noisy_frame, valid, scaled_noise = snr_mixer(data_frame, noise_snippet, SNR)
+        if valid:
+            noisy_feat = np.abs(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            noisy_angle = np.angle(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            clean_feat = np.abs(librosa.stft(data_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            noise_feat = np.abs(librosa.stft(scaled_noise, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            # normalize to 0-1.0
+            noisy_feat = noisy_feat / np.amax(noisy_feat)
+            clean_feat = clean_feat / np.amax(clean_feat)
+            noise_feat = noise_feat / np.amax(noise_feat)
+            mask =  tf.truediv(clean_feat, noisy_feat)
+            feat_in.append(quantize_spectrum(noisy_feat))
+            feat_out.append(quantize_spectrum(clean_feat))
+            noise_out.append(quantize_spectrum(noise_feat))
+            # is the recoding used in training (0) or test (1)
+            data_type = (np.random.rand(1) + 0.1).astype(int)
+            data_types.append(data_type)
+            angles.append(noisy_angle)
+            mask_out.append(quantize_spectrum(mask))
+print(feat_in.shape)
 
 speech_dataset = (feat_in, feat_out, angles, mask_out, noise_out, data_types)
 torch.save(speech_dataset, os.path.join(processed_folder, data_file))
