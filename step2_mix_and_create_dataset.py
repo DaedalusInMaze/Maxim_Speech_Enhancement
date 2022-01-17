3dc65d4524f8e39e688097a5b5973bc97e865f25
Batuhan Gundogdu <63497830+batuhan-gundogdu@users.noreply.github.com>
2022-01-16 18:42:11 -0600

    debugging

--- step2_mix_and_create_dataset.py
+++ step2_mix_and_create_dataset.py
@@ -58,37 +58,38 @@ for noise_type in noise_types:
             # is the recoding used in training (0) or test (1)
             angles.append(noisy_angle)
             mask_out.append(quantize_spectrum(mask))
-print(f'{len(feat_in)} recordings with {noise_type} noise in the training set')
-speech_dataset = (feat_in, feat_out, angles, mask_out, noise_out)
-torch.save(speech_dataset, os.path.join(processed_folder, data_filename))
-print(f'{os.path.join(processed_folder, data_filename)} created')
 
-test_feat_in = []
-test_angles = []
-test_feat_out = []
-test_mask_out = []
-test_noise_out = []
+    print(f'{len(feat_in)} recordings with {noise_type} noise in the training set')
+    speech_dataset = (feat_in, feat_out, angles, mask_out, noise_out)
+    torch.save(speech_dataset, os.path.join(processed_folder, data_filename))
+    print(f'{os.path.join(processed_folder, data_filename)} created')
 
-for count, data_frame in tqdm(enumerate(test)):
-    noise_start = random.randint(0, noise.shape[0]- data_frame.shape[0])
-    noise_snippet = noise[noise_start: noise_start + data_frame.shape[0]]
-    noisy_frame, valid, scaled_noise = snr_mixer(data_frame, noise_snippet, SNR)
-    if valid:
-        noisy_feat = np.abs(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        noisy_angle = np.angle(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        clean_feat = np.abs(librosa.stft(data_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        noise_feat = np.abs(librosa.stft(scaled_noise, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
-        # normalize to 0-1.0
-        noisy_feat = noisy_feat / np.amax(noisy_feat)
-        clean_feat = clean_feat / np.amax(clean_feat)
-        noise_feat = noise_feat / np.amax(noise_feat)
-        mask =  tf.truediv(clean_feat, noisy_feat)
-        test_feat_in.append(quantize_spectrum(noisy_feat))
-        test_feat_out.append(quantize_spectrum(clean_feat))
-        test_noise_out.append(quantize_spectrum(noise_feat))
-        test_angles.append(noisy_angle)
-        test_mask_out.append(quantize_spectrum(mask))
+    test_feat_in = []
+    test_angles = []
+    test_feat_out = []
+    test_mask_out = []
+    test_noise_out = []
 
-test_dataset = (test_feat_in, test_feat_out, test_angles, test_mask_out, test_noise_out)
-torch.save(test_dataset, os.path.join(processed_folder, test_filename))
+    for count, data_frame in tqdm(enumerate(test)):
+        noise_start = random.randint(0, noise.shape[0]- data_frame.shape[0])
+        noise_snippet = noise[noise_start: noise_start + data_frame.shape[0]]
+        noisy_frame, valid, scaled_noise = snr_mixer(data_frame, noise_snippet, SNR)
+        if valid:
+            noisy_feat = np.abs(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            noisy_angle = np.angle(librosa.stft(noisy_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            clean_feat = np.abs(librosa.stft(data_frame, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            noise_feat = np.abs(librosa.stft(scaled_noise, n_fft=K, win_length=N_d, hop_length=N_s, window='hanning').T)
+            # normalize to 0-1.0
+            noisy_feat = noisy_feat / np.amax(noisy_feat)
+            clean_feat = clean_feat / np.amax(clean_feat)
+            noise_feat = noise_feat / np.amax(noise_feat)
+            mask =  tf.truediv(clean_feat, noisy_feat)
+            test_feat_in.append(quantize_spectrum(noisy_feat))
+            test_feat_out.append(quantize_spectrum(clean_feat))
+            test_noise_out.append(quantize_spectrum(noise_feat))
+            test_angles.append(noisy_angle)
+            test_mask_out.append(quantize_spectrum(mask))
+
+    test_dataset = (test_feat_in, test_feat_out, test_angles, test_mask_out, test_noise_out)
+    torch.save(test_dataset, os.path.join(processed_folder, test_filename))
 print('Done')
